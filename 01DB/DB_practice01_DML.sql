80532588cad54543469d928efed5ce8f16918cca
akafintech <kyeong00416@gmail.com>
2025-02-25 22:43:47 +0900

    ins_homework

--- 01DB/DB_practice01_DML.sql
+++ 01DB/DB_practice01_DML.sql
@@ -124,3 +124,91 @@ SELECT *
 FROM passenger p
 INNER JOIN ticket t ON p.PassengerId = t.PassengerId
 INNER JOIN survived s ON p.PassengerId = s.PassengerId;
+
+# 1. passenger, ticket, survived 테이블을 조인하고 Survived가 1인 사람들만 찾아서 Name, Age, Sex, Pclass, survived 컬럼을 출력하시오.
+# SELECT * FROM passenger, ticket, survived limit 3;
+SELECT Name, Age, Sex, Pclass, survived
+FROM passenger p
+INNER JOIN ticket t ON p.passengerId = t.passengerId
+INNER JOIN survived s ON p.passengerId = s.passengerId
+where survived = 1;
+
+#2. 1의 결과를 10개만 출력하시오.
+SELECT Name, Age, Sex, Pclass, survived
+FROM passenger p
+INNER JOIN ticket t ON p.passengerId = t.passengerId
+INNER JOIN survived s ON p.passengerId = s.passengerId
+where survived = 1 LIMIT 10;
+
+#3. Passenger 테이블을 기준 ticket, survived테이블을 LEFT JOIN 한 결과에서 성별이 여성이면서 Pclass가 1인 사람 중 생존자(survived=1)를 찾아 이름, 성별, Pclass를 표시하시오.
+SELECT Name, Sex, Pclass
+FROM Passenger p
+LEFT JOIN ticket t ON p.PassengerId = t.PassengerId
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE Sex="female" AND Pclass = 1 AND survived = 1;
+
+#4. passenger, ticket, survived 테이블을 left join 후 나이가 10세 이상 20세 이하 이면서 Pclass 2인 사람 중 생존자를  표시하시오.
+SELECT *
+FROM Passenger p
+LEFT JOIN ticket t ON p.PassengerId = t.PassengerId
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE Age BETWEEN 10 AND 20 AND Pclass = 2 AND survived = 1;
+
+#5. passenger, ticket, survived 테이블을 left join 후 성별이 여성 또는 Pclass 가 1인 사람 중 생존자를 표시하시오.
+SELECT *
+FROM Passenger p
+LEFT JOIN ticket t ON p.PassengerId = t.PassengerId
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE survived = 1 AND (Sex="female" OR Pclass = 1);
+
+#6.  passenger, ticket, survived 테이블을 left join 후 생존자 중에서 이름에 Mrs가 포함된 사람을 찾아 이름, Pclass, 나이, Parch, Survived 를 표시하시오.
+SELECT Name, Pclass, Age, Parch, Survived
+FROM Passenger p
+LEFT JOIN ticket t ON p.PassengerId = t.PassengerId
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE Survived = 1 AND Name LIKE "%Mrs%";
+
+#7. passenger, ticket, survived 테이블을 left join 후 Pclass가 1, 2이고 Embarked가 s, c 인 사람중에서 생존자를 찾아 이름, 성별, 나이를 표시하시오.
+# SELECT * FROM t_info, p_info;
+
+SELECT Name, Sex, Age
+FROM Passenger p
+LEFT JOIN ticket t ON p.PassengerId = t.PassengerId
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE Pclass IN(1, 2) AND Embarked IN("s" ,"c") AND Survived = 1;
+
+-- 8. passenger, ticket, survived 테이블을 left join 후 이름에 James가 들어간 사람중 생존자를 찾아 이름, 성별, 나이 를 표시하고 나이를 기준으로 내림차순 정렬하시오.
+SELECT Name, Sex, Age
+FROM Passenger p
+LEFT JOIN ticket t ON p.PassengerId = t.PassengerId
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE Name LIKE "%James%" AND survived = 1 ORDER BY Age DESC;
+
+-- 9. passenger, ticket, survived 테이블을 INNER JOIN한 데이터에서 성별별, 생존자의 숫자를 구하시오. 생존자 숫자 결과는 별칭을 Total로 하시오.
+SELECT Sex, count(*) Total # count(*), count(survived), count(s.survived) 차이와 정확도 질문 할 것
+FROM Passenger p
+INNER JOIN ticket t ON p.PassengerId = t.PassengerId
+INNER JOIN Survived s ON p.PassengerId = s.PassengerId
+WHERE s.Survived = 1 GROUP BY Sex;
+#SELECT 기준컬럼명, 그룹연산 함수 FROM 테이블명 WHERE 컬럼명 GROUP BY 기준컬럼;
+
+-- 10. passenger, ticket, survived 테이블을 INNER JOIN한 데이터에서 성별별, 생존자의 숫자, 생존자 나이의 평균을 구하시오. 생존자 숫자 결과는 별칭을 Total로 하시오.
+SELECT Sex, AVG(Age), count(*) Total
+FROM Passenger p
+INNER JOIN ticket t ON p.PassengerId = t.PassengerId
+INNER JOIN Survived s ON p.PassengerId = s.PassengerId
+WHERE s.Survived = 1 GROUP BY Sex;
+
+-- 11. passenger, ticket, survived 테이블을 INNER JOIN한 데이터에서
+-- 성별별, pclass별, 생존자별로
+-- pclass, sex, survived ,
+-- survived의 클래스별 합계, 생존자/사망자의 나이 평균을 구하시오.
+-- survived의 별칭은 is_survived로,
+-- 생존자 클래스별 합계는 별칭을 survived_total로,
+-- 생존자/사망자의 나이 평균은 별칭을 avg_age로 하시오.
+# 문제 이해못함.......
+-- SELECT Pclass, Sex, Survived, AVG(s.Survived)
+-- FROM Passenger p
+-- INNER JOIN ticket t ON p.PassengerId = t.PassengerId
+-- INNER JOIN Survived s ON p.PassengerId = s.PassengerId
+-- WHERE s.Survived IN (1,2) GROUP BY Sex, Pclass, survived;
